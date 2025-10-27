86b7d5d71d2638f908206874ad801377f1fdd898
p100mma <82678610+p100mma@users.noreply.github.com>
2025-10-27 11:49:28 +0100

    Add files via upload

---
+++ arkusz_3/for_ex.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+// for ( instrukcje poczatkowe ;co pomiedzy iteracjami; WARUNEK ) {
+// instrukcje wykonywane az WARUNEK to true
+// }
+for (int i=0; i<8;i++){
+    printf("%d \n", i);
+}
+for (int i=0; i<8;i++){
+    printf("#");
+}
+// printf("%d \n", i); ERROR, i istnieje tylko w petli
+// n! = 1* 2 * 3* 4...*n
+int fac=1;
+for (int i=1; i<=4; i++){
+    fac*=i;
+}
+printf("\n !%d = %d ",4,fac);
+return 0;
+}
