d089d743daf71c992fc36a45052f1ed6f9a19f3c
p100mma <82678610+p100mma@users.noreply.github.com>
2025-10-27 11:42:38 +0100

    Add files via upload

---
+++ arkusz_3/ipp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+int i;
+i=1;
+i++;  // i= i +1;
+printf("%d \n",i);
+i--; //i = i -1
+printf("%d \n",i);
+i+=3; // i = i + 3
+printf("%d \n",i);
+i*=2; // i = i*2
+printf("%d \n",i);
+return 0;
+}
