194a50ce7d86d6194884f4e2431667baf91a2e41
p100mma <82678610+p100mma@users.noreply.github.com>
2025-12-08 12:47:52 +0100

    Add files via upload

---
+++ arkusz_5/chars.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+char x='b'; // char
+//if (strcmp(x, "b")==0){ printf("TRUE z strcmp \n");}; ERROR
+if (x=='b') printf("TRUE z x=='b'\n"); // tylko to OK jesli x to char
+char x2[]= "b";  //vs string - 1 elementowa tablica
+if (strcmp(x2,"b")==0) printf(" powinno sie uruchomic \n");
+if (strcmp(x, 'b')==0){ printf("TRUE z strcmp(x, 'b') \n");};  //nie pojdzie
+if (x2=='b') printf("nie powinno sie uruchomic \n"); //te dwa tez
+if (strcmp(x2,'b')) printf("nie powinno sie uruchomic \n");
+return 0;
+
+}
