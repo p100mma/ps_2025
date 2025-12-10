a95c5658431c8860472a04383b12effdadab4512
p100mma <82678610+p100mma@users.noreply.github.com>
2025-12-10 13:52:54 +0100

    Add files via upload

---
+++ arkusz_6/sscanf.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+char name[40];
+char species[40];
+int n_legs;
+double height_m;
+} Animal;
+
+void printAnimal( const Animal* x  ){
+printf("imie: %s \n", x->name);
+printf("gatunek: %s \n", x->species);
+printf("l. konczyn: %d \n", x->n_legs);
+printf("wys(m): %.2lf \n", x->height_m);
+}
+
+int main(){
+char linijka[]="gacek cat 4 0.5";
+char name[40];
+char spec[40];
+int n_konczyn;
+double wys;
+sscanf(linijka, "%s %s %d %lf", name, spec, &n_konczyn, &wys);
+printf("name= %s \n", name);
+printf("species= %s \n", spec);
+printf("n_konczyn= %d \n", n_konczyn);
+printf("wys= %.2lf \n", wys);
+
+Animal* wsk_kotek;
+Animal kotek;
+
+wsk_kotek= &kotek;
+strcpy(wsk_kotek->name, name);
+strcpy(wsk_kotek->species, spec);
+wsk_kotek->n_legs=n_konczyn;
+wsk_kotek->height_m=wys;
+printf("\n");
+printAnimal( wsk_kotek );
+printf("\n");
+printAnimal(&kotek);
+
+return 0;
+}
