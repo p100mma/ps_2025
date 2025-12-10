a7ae19b918d3fa28ff92f255cc8fb9d08815e4c7
p100mma <82678610+p100mma@users.noreply.github.com>
2025-12-10 14:01:01 +0100

    Enhance sscanf usage with return value check
    Added scanf return value check and error handling for sscanf.

--- arkusz_6/sscanf.c
+++ arkusz_6/sscanf.c
@@ -21,15 +21,15 @@ char name[40];
 char spec[40];
 int n_konczyn;
 double wys;
-sscanf(linijka, "%s %s %d %lf", name, spec, &n_konczyn, &wys);
+int scanf_value= sscanf(linijka, "%s %s %d %lf", name, spec, &n_konczyn, &wys);
+printf("scanf_value(jak 4 to ok): %d", scanf_value);
+printf("\n");
 printf("name= %s \n", name);
 printf("species= %s \n", spec);
 printf("n_konczyn= %d \n", n_konczyn);
 printf("wys= %.2lf \n", wys);
-
 Animal* wsk_kotek;
 Animal kotek;
-
 wsk_kotek= &kotek;
 strcpy(wsk_kotek->name, name);
 strcpy(wsk_kotek->species, spec);
@@ -39,6 +39,13 @@ printf("\n");
 printAnimal( wsk_kotek );
 printf("\n");
 printAnimal(&kotek);
-
+// przypadek blednych danych dla sscanf
+char line2[]="gacek pies duzy pies 4 1.1";
+char name2[40];
+char spec2[40];
+int n_konczyn2;
+double wys2;
+int scanf_value2= sscanf(line2, "%s %s %d %lf", name2, spec2, &n_konczyn2, &wys2);
+printf("scanf_value2(jak 4 to ok): %d", scanf_value2);
 return 0;
 }
